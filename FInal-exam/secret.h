42f18c8ce8eeb2cca5f719e64730f0efb1c724b5
Giacomo Cerretini <cerre.giacomo93@gmail.com>
2022-07-03 20:10:51 +0200

    Update secret.h

--- FInal-exam/secret.h
+++ FInal-exam/secret.h
@@ -1,4 +1,4 @@
 //API TS + wifi settings (ssid - password) + server TS
-String apiKey = "18HZC233MNXJ3YE2";
-const char* ssid = "iPhone di Giacomo";
-const char* password = "qwerty123";
+String apiKey = "API";
+const char* ssid = "connection-name";
+const char* password = "password";
